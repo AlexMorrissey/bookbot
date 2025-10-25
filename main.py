34e2efa6bd1cac8201d924896ba62301ac660544
AlexMorrissey <nukkels1@gmail.com>
2025-10-25 10:43:42 -0700

    updated stats.py to count characters and sort

---
+++ main.py
@@ -0,0 +1,33 @@
+from stats import get_num_words, get_num_letters, letters_sorted
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_letters = get_num_letters(text)
+    letters_sorted_list = letters_sorted(num_letters)
+    print_report(book_path, num_words, letters_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, letters_sorted_list):
+    print("========== BOOKBOT ==========")
+    print(f"Analyzing book found at {book_path}...")
+    print("---------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("---------- Character Count ----------")
+    for item in letters_sorted_list:
+        if not item["letter"].isalpha():
+            continue
+        print(f"{item["letter"]}: {item["num"]}")
+
+    print("========== END ==========")
+
+
+if __name__ == "__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,23 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_num_letters(text):
+    letters = {}
+    for letter in text:
+        letter = letter.lower()
+        if letter not in letters:
+            letters[letter] = 0
+        letters[letter] += 1
+    return letters
+
+def sort_on(dict):
+    return dict["num"]
+
+def letters_sorted(num_letters_dict):
+    letters_list = []
+    for let in num_letters_dict:
+        letters_list.append({"letter": let, "num": num_letters_dict[let]})
+    letters_list.sort(reverse=True, key=sort_on)
+    return letters_list
